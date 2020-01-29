5404cc5303a47e324d4390647dff44158f4fa6d8
katia <ktia_kurenai@hotmail.com>
2020-01-28 21:12:06 -0400

    Nodemon & Sucrase

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require('express')
+import { Router } from 'express'
 
 const routes = new Router()
 
 routes.get('/', (req, res) => {
-    return res.json({ message: 'Olar' })
+    return res.json({ message: 'Olar katia' })
 })
 
-module.exports = routes
+export default routes
