6d7c8c8472982f031d4fcb0e1051d38609c80e13
kahkurenai <sys1@grupopmz.com.br>
2020-01-30 09:37:43 -0400

    Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/Users';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Olar katia' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Katia Xavier',
+    email: 'katia@kurenai.com',
+    password_hash: '123',
+  });
+  res.json(user);
+});
 
 export default routes;
