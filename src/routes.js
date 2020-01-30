68507d9cace639cfe5954771fef93f1f400c1ecb
kahkurenai <sys1@grupopmz.com.br>
2020-01-30 13:04:42 -0400

    Cadastro de usuarios

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/Users';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Katia Xavier',
-    email: 'katia@kurenai.com',
-    password_hash: '123',
-  });
-  res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
